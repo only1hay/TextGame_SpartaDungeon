98ca6265c4357ad41a4bf0cd39fea16011c0a8f9
only1hay <only1hay@nate.com>
2024-01-04 21:47:38 +0900

    Update Program.cs

--- TextGame_SpartaDungeon/Program.cs
+++ TextGame_SpartaDungeon/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace TextGame_SpartaDungeon
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace TextGame_SpartaDungeon
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void intro()
         {
             Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
             Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
@@ -13,10 +15,56 @@
             Console.WriteLine("3. 상점");
             Console.WriteLine();
 
-            Console.WriteLine("원하시는 행동을 선택해 주세요.");
-            Console.WriteLine(">> "+ Console.ReadLine());
-
+            Console.WriteLine("원하시는 행동을 입력해 주세요.");
+            Console.Write(">> ");
             
+        }
+
+        static void outText()  
+        {
+            Console.WriteLine("원하시는 행동을 입력해 주세요.");
+            Console.Write(">> ");
+        }
+
+        static void states()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            intro();
+            int inputNum = int.Parse(Console.ReadLine());
+
+            if ( inputNum == 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("상태보기");
+                Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+
+                Console.WriteLine();
+                Console.WriteLine("0. 나가기");
+                outText();
+                int ioutNum = int.Parse(Console.ReadLine());
+            
+
+            }
+            else if (inputNum == 2)
+            {
+                Console.WriteLine("인벤토리");
+                Console.WriteLine("보유 증인 아이템을 관리할 수 있습니다.");
+            }
+            else if (inputNum == 3)
+            {
+                Console.WriteLine("상점");
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+
+
         }
     }
 }
